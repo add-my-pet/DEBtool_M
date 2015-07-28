a71a5648bbeab47e3f4bf04bf68fa44f0ec815ae
Starrlight Augustine <starrlight.augustine@akvaplan.niva.no>
2015-07-28 16:19:11 +0200

    some small changes
    - removed an empty useless folder
    -removed a function which was outside of all folders

--- WORKING_FOLDER/README.md
+++
@@ -1,3 +0,0 @@
-This is a working directory. Just for testing.
-
-If you read this, it means that I succeeded with GitHub :-)
--- lib/pet/mydata_my_pet.m
+++ lib/pet/mydata_my_pet.m
@@ -11,7 +11,7 @@ function [data, txt_data, metadata] = mydata_my_pet
   
   %% Description
   % Sets data, pseudodata, metadata, explanatory text, weight coefficients.
-  % Meant to be a template in add_my_pet
+  % Meant to be a template in add-my-pet
   %
   % Output
   %
@@ -67,14 +67,14 @@ metadata.address  = {'affiliation, zipcode, country'};   % affiliation, postcode
 % here assumed to be equal for all real data; the value of f is specified in pars_init_my_pet.
 
 % age 0 is at onset of embryo development
-data.ab = 15;      units.ab = 'd';    label.ab = 'age at birth';                bibkey.ab = 'MollCano2010';    
+data.ab = 15;      units.ab = 'd';    label.ab = 'age at birth';  bibkey.ab = 'MollCano2010';   comment.ab  = 'mean value taken from several measurements'; 
   temp.ab = T_C + 20;  % K, temperature 
   % observed age at birth is frequently larger than ab, because of diapauzes during incubation
-data.ap = 261;     units.ap = 'd';    label.ap = 'age at puberty';              bibkey.ap = 'Anon2015';
+data.ap = 261;     units.ap = 'd';    label.ap = 'age at puberty'; bibkey.ap = 'Anon2015';
   temp.ap = T_C + 20;  % K, temperature 
   % observed age at puberty is frequently larger than ap, 
   %   because allocation to reproduction starts before first eggs appear
-data.am = 591;     units.am = 'd';    label.am = 'life span';                   bibkey.am = 'Wiki';   
+data.am = 591;     units.am = 'd';    label.am = 'life span';     bibkey.am = 'Wiki';   
   temp.am = T_C + 20;  % K, temperature 
 % (accounting for aging only) 
 
@@ -83,7 +83,7 @@ data.am = 591;     units.am = 'd';    label.am = 'life span';                   
 % add an optional comment structure to give any additional explanations on
 % how the value was chosen, see the last column of the Lb data set for an
 % example
-data.Lb  = 0.45;   units.Lb  = 'cm';   label.Lb  = 'snout to vent length at birth';    bibkey.Lb  = 'Anon2015'; comment.Lb  = 'mean value taken from several measurements';
+data.Lb  = 0.45;   units.Lb  = 'cm';   label.Lb  = 'snout to vent length at birth';    bibkey.Lb  = 'Anon2015'; 
 data.Lp  = 2.36;   units.Lp  = 'cm';   label.Lp  = 'snout to vent length at puberty';  bibkey.Lp  = {'Anon2015','Wiki'}; % for multiple references, please use commas to separate references
 data.Li  = 6.25;   units.Li  = 'cm';   label.Li  = 'ultimate snout to vent length';    bibkey.Li  = 'Wiki';
 data.Wdb = 5.8e-5; units.Wdb = 'g';    label.Wdb = 'dry weight at birth';              bibkey.Wdb = 'Anon2015';
