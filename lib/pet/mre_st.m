7d147deb85c9844e3bc548f1cd8d823e7f6ee839
Starrlight Augustine <starrlight.augustine@akvaplan.niva.no>
2015-07-30 13:58:17 +0200

    unfinished mre_st
    some questions for Goncalo

--- lib/pet/mre_st.m
+++ lib/pet/mre_st.m
@@ -14,8 +14,7 @@ function [merr, rerr] = mre_st(func, par, data, auxData, weights)
   %
   % Input
   %
-  % * func: character string with name of user-defined function
-  %    see nrregr_st or nrregr
+  % * func: character string with name of user-defined predict_my_pet function
   % * par: structure with parameters
   % * data: structure with data
   % * auxData: structure with data
@@ -26,27 +25,50 @@ function [merr, rerr] = mre_st(func, par, data, auxData, weights)
   % * merr: scalar with mean absolute relative error
   % * rerr: vector with relative error of each of data set
 
-if isfield(data, 'psd')
-data = rmfield_wtxt(data, 'psd');
-end
+  % this is because pseudo data are appended to the data structure in
+  % add-my-pet routines and we are not interested in computing the relative error of pseudo-data :
+% if isfield(data, 'psd')
+% data = rmfield_wtxt(data, 'psd');
+% end
 
 [nm, nst] = fieldnmnst_st(data); % nst: number of data sets
-  for i = 1:nst   % makes st only with dependent variables
-    eval(['[~, k] = size(data.', nm{i}, ');']); 
+% multiSpecies = isempty(strfind(nm,'pet')); % 
+% 
+% switch multiSpecies
+% 
+%     case ~multiSpecies
+%     
+%     
+%     otherwise        
+%         
+% end
+
+  for i = 1:nst   % removes independant variables from uni-variate data sets
+      fieldsInCells = textscan(nm{i},'%s','Delimiter','.');
+      auxVar = getfield(data, fieldsInCells{1}{:});   % data in field nm{i}
+      k = size(auxVar, 2);
     if k >= 2
-      eval(['data.', nm{i}, ' = data.', nm{i},'(:,2);']);
+%        data.(nm{i}) = data.(nm{i})(:,2);
+var = data.(fieldsInCells{1}{1}).(fieldsInCells{1}{2});
+var = var(:,2);
+data.(fieldsInCells{1}{1}).(fieldsInCells{1}{2}) = var;
+% data.(fieldsInCells{1}{1}).(fieldsInCells{1}{2}) = data.(fieldsInCells{1}{1}).(fieldsInCells{1}{2})(:,2);
     end
   end
 
-  % get function values
-  f = feval(func, par, data, auxData);
+% call predicted values for all of the data
+prdData = feval(func, par, data, auxData);
 
-  rerr = [];
+  rerr = zeros(nst, 2);
   for i = 1:nst
-    % rerr = [rerr; sum(abs(fi - yi) .* wi ./ max(1e-3, yi), 1)/ sum(wi) 1];
-    eval(['rerr = [rerr; sum(abs(f.', nm{i}, ' - data.', nm{i}, ') .* weights.', nm{i}, ...
-          ' ./ max(1e-3, abs(data.', nm{i}, ')), 1)/ sum(max(1e-6, weights.', nm{i}, ')) 1];']);
-    eval(['rerr(end,2) = sum(weights.', nm{i}, ') ~= 0;']) % weight 0 if all 0 == yi(:,3)
+      fieldsInCells = textscan(nm{i},'%s','Delimiter','.');
+      var    = data.(fieldsInCells{1}{1}).(fieldsInCells{1}{2});
+      prdVar = prdData.(fieldsInCells{1}{1}).(fieldsInCells{1}{2});
+      w      = weights.(fieldsInCells{1}{1}).(fieldsInCells{1}{2});
+      rerr(i,1) = sum(abs(prdVar - var) .* w ./ max(1e-3, abs(var)), 1)/ sum(max(1e-6, w));
+      rerr(i,2) = (sum(w)~=0); % weight 0 if all of the data points in a data set were given wieght zero, meaning that that data set was effectively excluded from the estimation procedure
+%       rerr(i,1) = sum(abs(prdData.(nm{i}) - data.(nm{i})) .* weights.(nm{i}) ./ max(1e-3, abs(data.(nm{i}))), 1)/ sum(max(1e-6, weights.(nm{i})));
+%       rerr(i,2) = (sum(weights.(nm{i}))~=0); % weight 0 if all of the data points in a data set were given wieght zero, meaning that that data set was effectively excluded from the estimation procedure
   end
     
   merr = sum(prod(rerr,2))/ sum(rerr(:,2));
