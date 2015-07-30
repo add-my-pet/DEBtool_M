a1a46ba29561f8392ca8087928b3b8c3553a039b
Starrlight Augustine <starrlight.augustine@akvaplan.niva.no>
2015-07-30 16:25:10 +0200

    Revert "irrelevant"
    This reverts commit 3dbdf79670977b3ca77ea9d386cfc57fa7c1bb81.

--- lib/pet/run_my_pet.m
+++ lib/pet/run_my_pet.m
@@ -2,13 +2,12 @@
 % created by Starrlight Augustine, Bas Kooijman, Dina Lika, Goncalo Marques and Laure Pecquerie 2015/01/22
 % modified 2015/07/08
 
-clear all; 
-clc
+%clear all; 
 global pets
 
 % species names
 pets = {'my_pet'};
-% check_my_pet(pets); % check pet-files for required fields
+check_my_pet(pets); % check pet-files for required fields
 
 % See estim_options for more options
 estim_options('default'); % runs estimation, uses nmregr method and filter
@@ -25,8 +24,8 @@ estim_options('default'); % runs estimation, uses nmregr method and filter
 estim_options('max_step_number',5e3); % set options for parameter estimation
 estim_options('max_fun_evals',5e3);  % set options for parameter estimation
 
-estim_options('pars_init_method', 1);
-estim_options('results_output', 0);
-estim_options('method', 'no');
+estim_options('pars_init_method', 0);
+estim_options('results_output', 1);
+%estim_options('method', 'no');
 
 estim_pars; % run estimation
