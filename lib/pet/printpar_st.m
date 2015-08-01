63e3359d5335362139c2848852dcc716641b85be
GMMarques <goncalo.marques@tecnico.ulisboa.pt>
2015-08-01 08:24:15 +0100

    Minor correction to printpar_st
    Added ', ' to parameter presentation

--- lib/pet/printpar_st.m
+++ lib/pet/printpar_st.m
@@ -40,7 +40,7 @@ free = par.free;                    % copy substructure
 parpl = rmfield_wtxt(par, 'free');  % remove substructure free from par
 [nm nst] = fieldnmnst_st(parpl);    % get number of parameter fields
 for j = 1:nst % scan parameter fields
-  str = [txtPar.label.(nm{j}), txtPar.units.(nm{j}), ' %3.4g %d \n']; 
+  str = [nm{j}, ', ', txtPar.units.(nm{j}), ' %3.4g %d \n']; 
   fprintf(str, parpl.(nm{j}), free.(nm{j}));
 end
 
