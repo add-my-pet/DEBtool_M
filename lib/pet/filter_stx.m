d39d62c5ac0fefe5fa9f6fe56689e4acf6f6c95a
GMMarques <goncalo.marques@tecnico.ulisboa.pt>
2015-08-24 11:47:59 +0100

    Added stx files
    Added filter and warning files for stx model

---
+++ lib/pet/filter_stx.m
@@ -0,0 +1,39 @@
+%% filter_stx
+% filters for allowable parameters of standard DEB model for mammals (with fetus and delay in start of embryo development) 
+
+%%
+function [filter, flag] = filter_stx(p)
+% created 2015/08/24 by Goncalo Marques
+
+%% Syntax
+% [filter, flag] = <../filter_stx.m *filter_stx*> (p)
+
+%% Description
+% Checks if parameter values are in the allowable part of the parameter
+%    space of standard DEB model with fetus without acceleration
+% Meant to be run in the estimation procedure
+%
+% Input
+%
+% * p: structure with parameters (see below)
+%  
+% Output
+%
+% * filter: 0 for hold, 1 for pass
+% * flag: indicator of reason for not passing the filter
+%
+%     0: parameters pass the filter
+%     1: some parameter is negative
+%     2: some kappa is larger than 1
+%     3: growth efficiency is larger than 1
+%     4: maturity levels do not increase during life cycle
+%     5: puberty cannot be reached
+
+%% Remarks
+%  The theory behind boundaries is discussed in 
+%    <http://www.bio.vu.nl/thb/research/bib/LikaAugu2013.html LikaAugu2013>.
+
+  filter = 0; flag = 0; % default setting of filter and flag
+  
+  [filter, flag] = filter_stf(p);
+  
