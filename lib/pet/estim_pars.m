2e584ea7d7b52d705608c4e9db68d0898d1ed6fe
GMMarques <goncalo.marques@tecnico.ulisboa.pt>
2015-08-25 15:30:45 +0100

    Multispecies estimation changes
    Modified estim_pars to accomodate multispecies estimation
    Added cov_rules_basic which computes the parameter sets of the species
    taking into account the first level covariation rules

--- lib/pet/estim_pars.m
+++ lib/pet/estim_pars.m
@@ -33,29 +33,49 @@ global pets pars_init_method method filter
 % set data
 [data, auxData, metaData, txtData, weights] = mydata_pets;
 
+petsnumber = length(pets);
+
+if petsnumber == 1
+  pars_initnm = ['pars_init_', pets{1}];
+  resultsnm = ['results_', pets{1}, '.mat'];
+else
+  pars_initnm = 'pars_init_group';
+  resultsnm = 'results_group.mat';
+end
+
 % set parameters
 if pars_init_method == 0
-  [par, metaPar, txtPar, flag] = get_pars(data.pet1, auxData.pet1, metaData.pet1);
+  if petsnumber == 1
+    [par, metaPar, txtPar, flag] = get_pars(data.pet1, auxData.pet1, metaData.pet1);
+  else
+    error(['    For multispecies estimation get_pars cannot be used (pars_init_method cannot be 0)']);
+  end
 elseif pars_init_method == 1
-  filenm = ['results_', pets{1}, '.mat'];
-  load(filenm, 'par');
-  [par2, metaPar, txtPar] = feval(['pars_init_', pets{1}], metaData.pet1);
+  load(resultsnm, 'par');
+  [par2, metaPar, txtPar] = feval(pars_initnm, metaData.pet1);
   if length(fieldnames(par.free)) ~= length(fieldnames(par2.free))
     fprintf('The number of parameters in pars.free in the pars_init and in the .mat file are not the same. \n');
     return;
   end
   par.free = par2.free;
 elseif pars_init_method == 2
-  [par, metaPar, txtPar] = feval(['pars_init_', pets{1}], metaData.pet1);
+  [par, metaPar, txtPar] = feval(pars_initnm, metaData.pet1);
 end
+
 % check parameter set if you are using a filter
 if filter
   filternm = ['filter_', metaPar.model];
-  [pass, flag]  = feval(filternm, par);
+  pass = 1;
+  for i = 1:petsnumber
+    [passSpec, flag]  = feval(filternm, mspec_rules_basic(par, 1));
+    if ~passSpec
+      fprintf('The seed parameter set is not realistic. \n');
+      print_filterflag(flag);
+    end
+    pass = pass * passSpec;
+  end
   if ~pass 
-    fprintf('The seed parameter set is not realistic. \n');
-    print_filterflag(flag);
-    return;
+    error(['    The seed parameter set is not realistic.']);
   end
 end
 
@@ -73,5 +93,12 @@ end
 results_pets(par, metaPar, txtPar, data, auxData, metaData, txtData, weights);
 
 if filter
-  feval(['warning_', metaPar.model], par);
+  if petsnumber == 1
+    feval(['warning_', metaPar.model], par);
+  else
+    cov_rulesnm = ['cov_rules_', metaPar.covRules];
+    for i = 1:length(pets)
+      feval(['warning_', metaPar.model], feval(cov_rulesnm, par,i));
+    end
+  end
 end
