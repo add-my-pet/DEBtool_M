c4174af95df5f0eb717e2bee28d47c6d353bdb5a
baskooijman <bas.kooijman@vu.nl>
2015-09-16 18:30:49 +0200

    new functions

--- animal/get_ep_min_j.m
+++ animal/get_ep_min_j.m
@@ -2,11 +2,11 @@
 % Gets scaled reserve density at with maturation ceases at puberty in case of acceleration
 
 %%
-function [ep info] = get_ep_min_j(p)
+function [ep, info] = get_ep_min_j(p)
   % created 2015/09/16 by Bas Kooijman
   
   %% Syntax
-  % [ep info] = <..get_ep_min_j.m *get_ep_min_j*> (p)
+  % [ep, info] = <..get_ep_min_j.m *get_ep_min_j*> (p)
   
   %% Description
   % Obtains the scaled reserve at birth for growth and maturation ceases at puberty in case of acceleration. 
@@ -31,19 +31,9 @@ function [ep info] = get_ep_min_j(p)
   %% Example of use
   % get_ep_min_j([.1 1 0 .001 0.01 .1])
   
-  g   = p(1); % energy investment ratio
-  k   = p(2); % k_J/ k_M, ratio of maturity and somatic maintenance rate coeff
-  lT  = p(3); % scaled heating length {p_T}/[p_M]Lm
-  vHb = p(4); % v_H^b = U_H^b g^2 kM^3/ (1 - kap) v^2; U_B^b = M_H^b/ {J_EAm}
-  vHj = p(5); % v_H^j = U_H^j g^2 kM^3/ (1 - kap) v^2; U_B^j = M_H^j/ {J_EAm}
-  vHp = p(6); % v_H^p = U_H^p g^2 kM^3/ (1 - kap) v^2; U_B^p = M_H^p/ {J_EAm}
-
-  [l_j, l_p, l_b] = get_lj(p, 1); s_M = l_j/ l_b; ep_0 = lT + l_p/ s_M;
-  [ep, fval, info] = fzero(@fnget_ep_min_j, ep_0, [], g, k, lT, vHb, vHj, vHp);
+  [l_j, l_p, l_b] = get_lj(p, 1); ep_0 = p(3) + l_p * l_b/ l_p;
+  %[ep, fval, info] = fzero(@fnget_ep_min_j, ep_0, [], p);
+  par = [[ep_0; p'], [1;0;0;0;0;0;0]];
+  ep = nrregr('fnget_ep_min_j', par, [0 0 0]); ep = ep(1);
   
-end
-
-function F = fnget_ep_min_j(f, g, k, lT, vHb, vHj, vHp)
-  [l_j, l_p, l_b] = get_lj([g, k, lT, vHb, vHj, vHp], f);
-  F = l_p - (f - lT) * l_j/ l_b;
 end
