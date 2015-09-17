074104bd06c9d24d229d3a913c302f228f8d061e
baskooijman <bas.kooijman@vu.nl>
2015-09-17 08:40:41 +0200

    replaces get_ep_min_metam

--- animal/get_ep_min_j.m
+++ animal/get_ep_min_j.m
@@ -27,13 +27,26 @@ function [ep, info] = get_ep_min_j(p)
   %    <http://www.bio.vu.nl/thb/research/bib/Kooy2010b.html the comments for DEB3>.
   % See <get_ep_min.html *get_ep_min*> for without acceletaton
   % See <get_eb_min.html *get_eb_min*> for minimum e at birth.
+  % Solves f for which l_i = (f - l_T) s_M equals l_p with s_M = l_j/ l_b
 
   %% Example of use
   % get_ep_min_j([.1 1 0 .001 0.01 .1])
   
-  [l_j, l_p, l_b] = get_lj(p, 1); ep_0 = p(3) + l_p * l_b/ l_p;
-  %[ep, fval, info] = fzero(@fnget_ep_min_j, ep_0, [], p);
-  par = [[ep_0; p'], [1;0;0;0;0;0;0]];
-  ep = nrregr('fnget_ep_min_j', par, [0 0 0]); ep = ep(1);
+  [l_j, l_p, l_b] = get_lj(p, 1); ep_0 = p(3) + l_p * l_b/ l_j;
+  [ep, fval, info] = fzero(@fnget_ep_min_j, ep_0, [], p);
+  if ep <= 0 || ep >= 1
+    info = 0;
+  end
   
 end
+
+% %% subfunction
+
+function F = fnget_ep_min_j(f, p) % F = 0 if l_i = l_p
+  [l_j, l_p, l_b] = get_lj(p, f);
+  F = l_p - (f - p(3)) * l_j/ l_b; % p(3) = l_T and l_i = (f - l_T) * s_M for s_M = l_j/ l_b
+  if isempty(F) % no solution from get_lj
+    F = 1/f;
+  end
+end
+
