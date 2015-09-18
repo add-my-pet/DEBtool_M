5605c6e8c86384c33aa0d64000e7d7e120a2c3e0
baskooijman <bas.kooijman@vu.nl>
2015-09-18 12:40:07 +0200

    manual

--- taxa/clade.m
+++ taxa/clade.m
@@ -23,8 +23,7 @@ function [members, taxon] = clade(taxa)
 %% Remarks
 % The root is Animalia. 
 % If this is the lowest common taxon, the output contains all species in the collection.
-% If the input cell string has length 1, clade is similar to <select.html
-% *select*>.
+% If the input cell string has length 1, clade is similar to <select.html *select*>.
 % The classification follows that of Wikipedia.
 
 %% Example of use
