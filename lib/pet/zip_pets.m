6f100e8570bb9d3a7b65ccb76774e538f4ee7fea
baskooijman <bas.kooijman@vu.nl>
2015-10-04 08:58:32 +0200

    zips selected species

---
+++ lib/pet/zip_pets.m
@@ -0,0 +1,48 @@
+%% zip_pets
+% Compress files of all species in a taxon in the current folder to the file taxon.zip.
+
+%%
+function zip_pets(taxon, basefolder)
+  % created by Bas Kooijman 2015/10/04
+
+  %% Syntax
+  % <../zip_pets.m *zip_pets*>(taxon, basefolder)
+  
+  %% Description  
+  % zips all files of all species in a taxon in file "taxon.zip" 
+  % If full path to the my_pet folder is not specified, 
+  %   the current folder is '.\' (one level up)
+  %
+  % Input
+  %
+  % * taxon: string with name of taxon
+  %
+  % * species: cell string with species
+  %
+  % * basefolder: String that specifies the root of the paths 
+  %               for the files to zip (optional)
+  %               Default folder:  '.\'
+  %
+  % Output 
+  %
+  % * zipped file in current folder  
+ 
+  %% Remarks
+  % See also zip_my_pet
+  
+  %% Example of use
+  % zip_pets('Aves')
+  
+  WD = pwd;                % store current path
+  taxa = which('select'); 
+  taxa = taxa(1:end - 8);  % path to DEBtool_M/taxa/
+  cd(taxa)                 % goto taxa
+  species = select(taxon); % select all species in a taxon
+  cd(WD)                   % goto original path
+
+  if nargin == 1
+    basefolder = '.'; % default path to the basefolder
+  end
+
+  % zip file is created in your current folder.
+  zip(taxon, species, basefolder)
