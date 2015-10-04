43f5950769d8e2d0d22f9b46ccabd57e3bf7ed66
baskooijman <bas.kooijman@vu.nl>
2015-10-04 12:35:41 +0200

    fix by Dina

--- animal/get_ep_min_j.m
+++ animal/get_ep_min_j.m
@@ -1,9 +1,9 @@
 %% get_ep_min_j
-% Gets scaled reserve density at with maturation ceases at puberty in case of acceleration
+% Gets scaled reserve density at which maturation ceases at puberty in case of acceleration
 
 %%
 function [ep, info] = get_ep_min_j(p)
-  % created 2015/09/16 by Bas Kooijman
+  % created 2015/09/16 by Bas Kooijman, Dina Lika
   
   %% Syntax
   % [ep, info] = <..get_ep_min_j.m *get_ep_min_j*> (p)
@@ -11,7 +11,6 @@ function [ep, info] = get_ep_min_j(p)
   %% Description
   % Obtains the scaled reserve at birth for growth and maturation ceases at puberty in case of acceleration. 
   % It can be seen as the lower viable scaled reserve density for reproduction. Cf get_eb_min.
-  % See get_ep_min for no acceletaton
   %
   % Input
   %
@@ -27,7 +26,7 @@ function [ep, info] = get_ep_min_j(p)
   %    <http://www.bio.vu.nl/thb/research/bib/Kooy2010b.html the comments for DEB3>.
   % See <get_ep_min.html *get_ep_min*> for without acceletaton
   % See <get_eb_min.html *get_eb_min*> for minimum e at birth.
-  % Solves f for which l_i = (f - l_T) s_M equals l_p with s_M = l_j/ l_b
+  % Solves f for which k v_Hp = f (f - lT) s_M^3 with s_M = l_j/ l_b
 
   %% Example of use
   % get_ep_min_j([.1 1 0 .001 0.01 .1])
@@ -44,7 +43,7 @@ end
 
 function F = fnget_ep_min_j(f, p) % F = 0 if l_i = l_p
   [l_j, l_p, l_b] = get_lj(p, f);
-  F = l_p - (f - p(3)) * l_j/ l_b; % p(3) = l_T and l_i = (f - l_T) * s_M for s_M = l_j/ l_b
+  F = f * (f - p(3))^2 * (l_j/ l_b)^3 - p(2) * p(6);
   if isempty(F) % no solution from get_lj
     F = 1/f;
   end
