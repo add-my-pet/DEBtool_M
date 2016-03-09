3021b921b09bc3f76c08cf3bdc0997b3bb7e449f
baskooijman <bas.kooijman@vu.nl>
2016-03-09 07:05:22 +0100

    solve printing _ issue

--- lib/misc/shlegend.m
+++ lib/misc/shlegend.m
@@ -13,9 +13,9 @@ function Hlegend = shlegend(legend, pos, space, txt, i_legend)
 %
 % Input:
 %
-% * legend: (n,2)-cell matrix with with marker (5-vector of cells), taxon (string)
+% * legend: (n,2)-cell matrix with with marker (5-vector of cells), item (string)
 % * pos: optional 2-vector with position of lower-left corner of legend within box
-% * space: optinal 2-vector with space between marker and taxon (horizontal) space between marker and marker (vertical)
+% * space: optinal 2-vector with space between marker and item (horizontal) space between marker and marker (vertical)
 % * txt: optional character string with title above legend figure
 % * i_legend: optional integer with highlighter (from 1 till n)
 %
@@ -53,10 +53,10 @@ plot([0 width width 0 0], [0 0 height height 0], 'k')
 hold on
 
 for i = 1:n
-  marker = legend{n-i+1,1}; taxon = legend(n-i+1,2);
+  marker = legend{n-i+1,1}; item = legend(n-i+1,2);
   T = marker{1}; MS = marker{2}; LW = marker{3}; MEC = marker{4}; MFC = marker{5};  
   plot(pos(1), pos(2), T, 'MarkerSize', MS, 'LineWidth', LW, 'MarkerFaceColor', MFC, 'MarkerEdgeColor', MEC); axis('off');
-  text(space_MT + pos(1), pos(2), taxon, 'Interpreter', 'none');
+  text(space_MT + pos(1), pos(2), strrep(item, '_', '\_'), 'Interpreter', 'tex');
   if exist('i_legend', 'var') && i_legend == n-i+1
     text(pos(1) - 1.5, pos(2), '>');
   end
