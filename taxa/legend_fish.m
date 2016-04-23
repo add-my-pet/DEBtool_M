1c0265a43838b5f3a6c9672cda0bd3be916d5218
baskooijman <bas.kooijman@vu.nl>
2016-04-23 11:48:40 +0200

    update

---
+++ taxa/legend_fish.m
@@ -0,0 +1,28 @@
+%% legend_fish
+% specification of a legend for fish
+
+%
+function legend = legend_fish
+% created at 2016/04/23 by Bas Kooijman
+
+  %% Syntax
+  % legend = <legend_fish.m *legend_fish*>
+
+  %% Description
+  % Specifies a legend for fish
+  %
+  % Output
+  %
+  % * legend: (7,2) cell matix with marker, taxon pairs
+
+  % type, size, linewidth, edge color and face color of a marker, taxon
+  legend = {...
+    {'o', 8, 3, [1 0 0], [1 0 0]}, 'Myxini'; ...
+    {'o', 8, 3, [1 0 0], [0 0 0]}, 'Agnatha'; ...
+    {'o', 8, 3, [0 1 0], [0 1 0]}, 'Chondrichthyes'; ....
+    {'o', 8, 3, [0 0 1], [0 0 1]}, 'Actinopterygii'; ....
+    {'o', 8, 3, [1 1 0], [1 1 0]}, 'Coelacanthiformes'; ....
+    {'o', 8, 3, [1 1 0], [0 0 0]}, 'Dipnoi'; ....
+    {'.', 8, 3, [0 0 0], [0 0 0]}, 'Animalia'; ....
+  };
+end
