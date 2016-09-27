bfbabffd8a37e36aa0b74cf03f1709634fe98a62
baskooijman <bas.kooijman@vu.nl>
2016-09-27 16:51:07 +0200

    fnknot placed in separate fn

--- lib/misc/knot.m
+++ lib/misc/knot.m
@@ -57,11 +57,3 @@ function [xy, info] = knot(x, data, Dy1, Dyk)
   nrregr_options('report',1);
 end
 
-% %% subfunction
-
-function f = fnknot(y, data)
-  % created at 2002/05/26 by Bas Kooijman
-  % subroutine for 'knot' to find smoothing spline ordinates
-  global X DY1 DYk
-  f = spline(data(:,1),[X,y],DY1, DYk);
-end
