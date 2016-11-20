f792a21662e6645712e4ed0b1365e06fb89169f5
baskooijman <bas.kooijman@vu.nl>
2016-11-20 09:13:04 +0100

    styling

--- lib/misc/shtemp2corr.m
+++ lib/misc/shtemp2corr.m
@@ -21,22 +21,19 @@ function shtemp2corr (T_1, Tpars)
   %% Example of use
   % clear all; pars_animal; shtemp2corr
   
-
-  global t_1 tpars;
-  t_1 = T_1; tpars = Tpars;
-
   T_A = Tpars(1); % Arrhenius temperature
   T_L = Tpars(2); % Lower temp boundary
   T_H = Tpars(3); % Upper temp boundary
-  %T_AL = Tpars(4); % Arrh. temp for lower boundary
-  %T_AH = Tpars(5); % Arrh. temp for upper boundary
+  T_AL = Tpars(4); % Arrh. temp for lower boundary
+  T_AH = Tpars(5); % Arrh. temp for upper boundary
 
   hold on;
   title ('temperature dependence of rates');
   xlabel('10^4/temp, K'); ylabel('log_{10} corr factor');
   
-  T_LL = fzero('findt', T_L);
-  T_HH = fzero('findt', T_H + 10);
+  % 5-parameter case
+  T_LL = fzero(@findt, T_L, [], T_1, Tpars);
+  T_HH = fzero(@findt, T_H + 10, [], T_1, Tpars);
   iT_L = linspace(1/T_LL, 1/T_L, 20);
   TC_L = tempcorr (1./iT_L, T_1, Tpars);
   iT   = linspace(1/T_L, 1/T_H, 60);
@@ -44,17 +41,37 @@ function shtemp2corr (T_1, Tpars)
   iT_H = linspace(1/T_H, 1/T_HH, 20);
   TC_H = tempcorr (1./iT_H, T_1, Tpars);
 
-  % semilogy(10^4*iT_H, TC_H, 'r', 10^4*iT, TC, 'g', 10^4*iT_L, TC_L, 'b');
-  % does not work;
   plot(10^4*iT_H, log10(TC_H), 'r', ...
-       10^4*iT, log10(TC), 'g', 10^4*iT_L, log10(TC_L), 'b');
+       10^4*iT, log10(TC), 'g', ...
+       10^4*iT_L, log10(TC_L), 'b');
   
-  
-  TC_L = exp(T_A/T_1 - T_A/T_L); TC_H = exp(T_A/T_1 - T_A/T_H);
-  % semilogy([10^4/T_L, 10^4/T_L], [0.01, TC_L], 'b', ...
-	%   [10^4/T_L, 10^4/T_H], [TC_L, TC_H], 'g', ...
-	%   [10^4/T_H, 10^4/T_H], [0.01, TC_H], 'r');
-  plot([10^4/T_L, 10^4/T_L], log10([0.01, TC_L]), 'b', ...
-	   [10^4/T_L, 10^4/T_H], log10([TC_L, TC_H]), 'g', ...
-	   [10^4/T_H, 10^4/T_H], log10([0.01, TC_H]), 'r');
+  % 3-parameter case low temp
+  Tpars = [T_A; T_L; T_AL];
+  TC_L = tempcorr (1./iT_L, T_1, Tpars);
+  iT   = linspace(1/T_L, 1/T_HH, 100);
+  TC   = tempcorr (1./iT, T_1, Tpars);
+  plot(10^4*iT, log10(TC), 'g', ...
+       10^4*iT_L, log10(TC_L), 'b');
+   
+  % 1-parameter case
+  iT   = linspace(1/T_LL, 1/T_HH, 120);
+  TC   = tempcorr (1./iT, T_1, T_A);
+  plot(10^4*iT, log10(TC), 'g');
 
+  % 3-parameter case high temp
+  iT   = linspace(1/T_LL, 1/T_H, 100); 
+  TC = exp(T_A/ T_1 - T_A .* iT) .* ...
+	     (1 + exp(T_AH/ T_H - T_AH  / T_1)) ./ ...
+	     (1 + exp(T_AH/ T_H - T_AH .* iT  ));
+  TC_H = exp(T_A/ T_1 - T_A .* iT_H) .* ...
+	     (1 + exp(T_AH/ T_H - T_AH  / T_1)) ./ ...
+	     (1 + exp(T_AH/ T_H - T_AH .* iT_H));
+  plot(10^4.*iT, log10(TC), 'g', ...
+       10^4.*iT_H, log10(TC_H), 'r');
+
+end
+
+function f = findt (T, T_1, Tpars)
+  % function to find temp for which tempcorr is 0.01
+  f = log(tempcorr(T, T_1, Tpars)) - log(0.01);
+end
--- lib/misc/tempcorr.m
+++ lib/misc/tempcorr.m
@@ -2,11 +2,11 @@
 % computes temperature correction factor
 
 %%
-function TC = tempcorr (T,T_1,Tpars)
-  %  Created at 2002/04/09 by Bas Kooijman; modified 2005/01/24
+function TC = tempcorr (T, T_ref, pars_T)
+  %  Created at 2002/04/09 by Bas Kooijman; modified 2005/01/24, 2016/11/18
   
   %% Syntax
-  % TC = <../tempcorr.m *tempcorr*> (T,T_1,Tpars)
+  % TC = <../tempcorr.m *tempcorr*> (T, T_ref, pars_T)
 
   %% Description
   %  Calculates the factor with which physiological rates should be multiplied 
@@ -15,8 +15,12 @@ function TC = tempcorr (T,T_1,Tpars)
   % Input
   %
   % * T: vector with new temperatures
-  % * T_1: scalar with reference temperature
-  % * Tpars: 1-, 3- or 5-vector with temperature parameters: T_A, T_L, T_H, T_AL, T_AH
+  % * T_ref: scalar with reference temperature
+  % * pars_T: 1-, 3- or 5-vector with temperature parameters
+  %
+  %    1: T_A
+  %    3: T_A, T_L, T_AL
+  %    5: T_A, T_L, T_H, T_AL, T_AH    
   %
   % Output:
   %
@@ -29,21 +33,23 @@ function TC = tempcorr (T,T_1,Tpars)
   %  tempcorr([330 331 332], 320, [12000 277 318 20000 190000]) and 
   %  shtemp2corr(320, [12000 277 318 20000 190000]). 
 
-  T_A = Tpars(1); % Arrhenius temperature
-  if length(Tpars) == 1
-    TC = exp(T_A/T_1 - T_A./T);
-  elseif length(Tpars) == 3
-    T_L = Tpars(2); % Lower temp boundary
-    T_AL = Tpars(3); % Arrh. temp for lower boundary
-    TC = exp(T_A/ T_1 - T_A./ T) * (1 + exp(T_AL./ T_1 - T_AL/ T_L)) ./ ...
-         (1 + exp(T_AL./ T - T_AL/ T_L));
+  T_A = pars_T(1); % Arrhenius temperature
+  if length(pars_T) == 1
+    TC = exp(T_A/ T_ref - T_A ./ T);
+    
+  elseif length(pars_T) == 3
+    T_L  = pars_T(2); % Lower temp boundary
+    T_AL = pars_T(3); % Arrh. temp for lower boundary
+    TC = exp(T_A/ T_ref - T_A ./ T) .* ...
+         (1 + exp(T_AL ./ T_ref - T_AL/ T_L)) ./ ...
+         (1 + exp(T_AL ./ T     - T_AL/ T_L));
   else
-    T_L = Tpars(2); % Lower temp boundary
-    T_H = Tpars(3); % Upper temp boundary
-    T_AL = Tpars(4); % Arrh. temp for lower boundary
-    T_AH = Tpars(5); % Arrh. temp for upper boundary
+    T_L  = pars_T(2);  % Lower temp boundary
+    T_H  = pars_T(3);  % Upper temp boundary
+    T_AL = pars_T(4);  % Arrh. temp for lower boundary
+    T_AH = pars_T(5);  % Arrh. temp for upper boundary
 
-    TC = exp(T_A/ T_1 - T_A./ T) * ...
-	(1 + exp(T_AL./ T_1 - T_AL/ T_L) + exp(T_AH/ T_H - T_AH./ T_1))./ ...
-	(1 + exp(T_AL./ T - T_AL/ T_L) + exp(T_AH/ T_H - T_AH./ T));
+    TC = exp(T_A/ T_ref - T_A ./ T) .* ...
+	     (1 + exp(T_AL ./ T_ref - T_AL/ T_L) + exp(T_AH/ T_H - T_AH ./ T_ref)) ./ ...
+	     (1 + exp(T_AL ./ T     - T_AL/ T_L) + exp(T_AH/ T_H - T_AH ./ T    ));
   end
