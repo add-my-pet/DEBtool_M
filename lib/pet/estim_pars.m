954d9a9f20826f421d7c9cc1c05064e4411ba8b9
Starrlight Augustine <starrlight.augustine@akvaplan.niva.no>
2018-08-17 09:51:44 +0200

    estim_pars
    removed toxs option
    edited header

--- lib/pet/estim_pars.m
+++ lib/pet/estim_pars.m
@@ -6,7 +6,7 @@ function nsteps = estim_pars
   % created 2015/02/10 by Goncalo Marques
   % modified 2015/02/10 by Bas Kooijman, 
   %   2015/03/31, 2015/07/30, 2017/02/03 by Goncalo Marques, 
-  %   2018/05/23 by Bas Kooijman
+  %   2018/05/23 by Bas Kooijman,     2018/08/17 by Starrlight Augustine
   
   %% Syntax 
   % <../estim_pars.m *estim_pars*>
@@ -25,27 +25,22 @@ function nsteps = estim_pars
   %  
   % Output
   %
-  % * no output
+  % * nsteps: number of steps
   
   %% Remarks
   % estim_options sets many options;
   % option filter = 0 selects filter_nat, which always gives a pass, but still allows for costomized filters in the predict file
   
-global pets toxs pars_init_method method filter covRules
+global pets pars_init_method method filter covRules
 
 n_pets = length(pets);
-n_toxs = length(toxs);
 
 % get data
 [data, auxData, metaData, txtData, weights] = mydata_pets;
 
 if n_pets == 1
   pars_initnm = ['pars_init_', pets{1}];
-  if n_toxs == 0
     resultsnm = ['results_', pets{1}, '.mat'];
-  else
-    resultsnm = ['results_', pets{1}, '_', toxs{1}, '.mat'];
-  end
 else
   pars_initnm = 'pars_init_group';
   resultsnm = 'results_group.mat';
@@ -55,13 +50,10 @@ end
 if pars_init_method == 0
   if n_pets ~= 1
     error('    For multispecies estimation get_pars cannot be used (pars_init_method cannot be 0)');
-  elseif n_toxs ~= 0
-    error('    For estimation with toxicant get_pars cannot be used (pars_init_method cannot be 0)');
   else
     [par, metaPar, txtPar] = get_pars(data.(pets{1}), auxData.(pets{1}), metaData.(pets{1}));
   end
 elseif pars_init_method == 1
-  if n_toxs == 0
     load(resultsnm, 'par');
     if n_pets == 1
       [par2, metaPar, txtPar] = feval(pars_initnm, metaData.(pets{1}));
@@ -73,21 +65,12 @@ elseif pars_init_method == 1
       return;
     end
     par.free = par2.free;
-  else
-    load(resultsnm, 'par', 'metaPar', 'txtPar');
-  end
 elseif pars_init_method == 2
-  if n_toxs == 0
     if n_pets == 1
       [par, metaPar, txtPar] = feval(pars_initnm, metaData.(pets{1}));
     else
       [par, metaPar, txtPar] = feval(pars_initnm, metaData);
     end
-  else
-    [~, ~, metaDataAux, ~, ~] = feval(['mydata_', pets{1}]);   % This is a provisory way of getting metaData for pars_init
-    [par, metaPar, txtPar] = feval(pars_initnm, metaDataAux);
-    [par, metaPar, txtPar] = feval([pars_initnm, '_', toxs{1}], par, metaPar, txtPar);
-  end
 end
 
 % make sure that global covRules exists
