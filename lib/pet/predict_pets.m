065acc196ec3a0d53f127e96109e3461014e0873
DinaLika <lika@biology.uoc.gr>
2018-08-20 10:36:48 +0300

    predict_pets - petTox removed

--- lib/pet/predict_pets.m
+++ lib/pet/predict_pets.m
@@ -23,28 +23,18 @@ function [prdData, info] = predict_pets(parGrp, data, auxData)
 % * prdData: structure with predictions for several pets
 % * info: scalar with combined success (1) or failure (0) of predictions
  
-global pets toxs 
+global pets  
 
 info = 0;
 parPets = parGrp2Pets(parGrp); % convert parameter structure of group of that of pets 
 
 % produce predictions
 n_pets = length(pets);
-n_toxs = length(toxs);
 for i = 1:n_pets
-  if n_toxs == 0
     [prdData.(pets{i}), info] = feval(['predict_', pets{i}], parPets.(pets{i}), data.(pets{i}), auxData.(pets{i}));
     if ~info
       return;
     end
-  else
-    for j = 1:n_toxs
-      [prdData.(pets{i}).(toxs{j}), info] = feval(['predict_', pets{i}, '_', toxs{j}], parPets.(pets{i}), data.(pets{i}).(toxs{j}), auxData.(pets{i}).(toxs{j}));
-      if ~info
-        return;
-      end
-    end
-  end
   
   % predict pseudodata
   prdData.(pets{i}) = predict_pseudodata(parPets.(pets{i}), data.(pets{i}), prdData.(pets{i}));
