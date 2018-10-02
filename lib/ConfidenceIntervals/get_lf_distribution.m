aa6d5d1bc913cbc6ea6e154e8781cfe3aeb8b0fa
DinaLika <lika@biology.uoc.gr>
2018-10-02 14:56:44 +0300

    ConfidenceInterval
    new folder that contains the templates for computing CI

---
+++ lib/ConfidenceIntervals/get_lf_distribution.m
@@ -0,0 +1,166 @@
+%% <../get_lf_distribution.m *get_lf_distribution*>
+% created by Dina Lika 2018/08/27
+  %% Syntax
+  % [p_vec, lf] = <../get_lf_distribution.m *get_lf_distribution*>(nTrials, nCont)
+  %
+  %% Description
+  % Calibration step for the interval estimation
+  % Calculates the global minima of the loss function, 
+  % that correspond with nTrials, say 500 or 1000, 
+  % Monte Carlo simulations of data that has a scatter equal to the observed scatter.
+  %
+  % The theory is discussed in Marques et al. 2018. "Fitting Multiple
+  % Models to Multiple Data Sets". J Sea Research, doi.org/10.1016/j.seares.2018.07.004
+  %
+  %% Input
+  %
+  % * nTrials: number of Monte Carlo data sets
+  % * nCont: number of continuations
+  %
+  %% Output
+  %
+  % * lf: vector with the values of the loss function
+  % * p_vec: matrix with the (free) parameter values
+  % * name_par: names of free parameters
+  %% Example of use
+  %  get_lf_distribution(10, 5); % for 10 MonteCarlo  trials and 5
+  %  continuations of the estimation 
+
+function [lf, p_vec, nm] = get_lf_distribution(nTrials, nCont)
+
+%% Script - Calibration step for the interval estimation
+% calls generate_data
+
+global pet
+
+% set all of the estimation options:
+estim_options('default'); 
+nmregr_options('report', 0);  % does not report to screen to save time
+
+[data, auxData, metaData, txtData, weights] = feval(['mydata_', pet]); 
+[par, metaPar, txtPar] = feval(['pars_init_', pet], metaData);
+[prdData, info] = feval(['predict_',pet], par, data, auxData); 
+filternm = ['filter_', metaPar.model];
+
+% compute cv to be used to generate new datasets
+% cv = the mean of the absolute difference between observed and predicted 
+% values divided by the predicted value
+st = data; 
+[nm, nst] = fieldnmnst_st(prdData); % nst: number of data sets, take the field from prdData to exclude pseudo-data
+cv_vec = zeros(nst,1);
+for i = 1:nst   % makes st only with dependent variables
+    fieldsInCells = textscan(nm{i},'%s','Delimiter','.');
+    auxVar = getfield(st, fieldsInCells{1}{:});   % data in field nm{i}
+    k = size(auxVar, 2);
+    if k >= 2
+        st = setfield(st, fieldsInCells{1}{:}, auxVar(:,2));
+    end
+    prdAux  = getfield(prdData, fieldsInCells{1}{:});
+    dataAux = getfield(st, fieldsInCells{1}{:});
+    cv_vec(i) = sum(abs(dataAux-prdAux)./prdAux)/length(prdAux);
+end
+cv = mean(cv_vec);
+
+% generate new datasets, estimate parameters and calculate the value of the
+% loss function
+p_vec=[]; lf = zeros(nTrials,1); 
+for j=1:nTrials
+     fprintf(['Trial ', num2str(j), '\n']);
+    [prdData, info] = feval(['predict_',pet], par, data, auxData);
+    newData = generate_data(data, prdData, cv);  % generate new datasets
+    % estimate parameters with the newData set, nCont continuations
+    for k =1:nCont
+        [par, info, nsteps] = petregr_f(@predict_data_psd, par, newData, auxData, weights, filternm);
+    end
+    [prdData, info] = predict_data_psd(par, newData, auxData);
+    lf(j) = lossfun(newData, prdData, weights); % calculate the value of the loss function
+    % save in p_vec only those that are estimated 
+    p = [];
+    [nm, npar] = fieldnmnst_st(par.free); % nst: number of pars 
+    for i = 1:npar   % makes st only with dependent variables
+        fieldsInCells = textscan(nm{i},'%s','Delimiter','.');
+        auxVar = getfield(par.free, fieldsInCells{1}{:});   % data in field nm{i}
+        auxPar = getfield(par, fieldsInCells{1}{:});   % data in field nm{i}
+        if auxVar == 1
+            p = setfield(p, fieldsInCells{1}{:}, auxPar);
+        end
+    end
+    % convert structure to vector
+    [nm, np] = fieldnmnst_st(p); % nst: number of pars 
+    [p_free, x] = struct2vector(p, nm);
+    p_vec = [p_vec, p_free]; % save parameter estimates
+    
+    save('intCalibrate', 'lf', 'p_vec', 'nm'); % save intermidiate values
+end
+end
+%% This function generates new data sets  
+% from log normal distribution with mean the estimated value and 
+%
+function newData = generate_data(data, prdData, cv)
+
+st = data; % take the field from prdData because data have also the pseudo-data
+[nm, nst] = fieldnmnst_st(prdData); % nst: number of data sets
+
+for i = 1:nst   % makes st only with dependent variables
+    fieldsInCells = textscan(nm{i},'%s','Delimiter','.');
+    auxVar = getfield(st, fieldsInCells{1}{:});   % data in field nm{i}
+    prdAux = getfield(prdData, fieldsInCells{1}{:}); 
+    k = size(auxVar, 2);
+    if k < 2 % zero-variate data
+        new = add_noise(prdAux,cv, 1);
+    else     % uni-variate data
+        new = zeros(length(prdAux), 2); 
+        for j = 1:length(prdAux)
+            new(j,2) = add_noise(prdAux(j),cv, 1); % dependent variable
+            new(j,1) = auxVar(j,1);                % independent variable
+        end      
+    end
+    newData.([nm{i}]) = new;
+end
+newData.psd = data.psd;  % add pseudodata
+end
+%% auxiliary functions
+function [prdData, info] = predict_data_psd(par, data, auxData)
+% Predictions, using parameters and data
+% Adds pseudodata predictions into predictions structure 
+
+global pet
+
+[prdData, info] = feval(['predict_',pet], par, data, auxData);
+prdData = predict_pseudodata(par, data, prdData);
+
+end
+%%
+function lf_val = lossfun(data, prdData, weights)
+% compute the value of the loss function
+
+global lossfunction
+
+    fileLossfunc = ['lossfunction_', lossfunction];
+    st = data; % take the field from prdData because data have also the pseudo-data
+    [nm, nst] = fieldnmnst_st(data); % nst: number of data sets
+  
+    for i = 1:nst   % makes st only with dependent variables
+        fieldsInCells = textscan(nm{i},'%s','Delimiter','.');
+        auxVar = getfield(st, fieldsInCells{1}{:});   % data in field nm{i}
+        k = size(auxVar, 2);
+    if k >= 2
+        st = setfield(st, fieldsInCells{1}{:}, auxVar(:,2));
+    end
+    end
+    [Y, meanY] = struct2vector(st, nm);
+    W = struct2vector(weights, nm);
+    [P, meanP] = struct2vector(prdData, nm);
+    lf_val = feval(fileLossfunc, Y, meanY, P, meanP, W);
+end
+%%
+function [vec, meanVec] = struct2vector(struct, fieldNames)
+  vec = []; meanVec = [];
+  for i = 1:size(fieldNames, 1)
+    fieldsInCells = textscan(fieldNames{i},'%s','Delimiter','.');
+    aux = getfield(struct, fieldsInCells{1}{:});
+    vec = [vec; aux];
+    meanVec = [meanVec; ones(length(aux), 1) * mean(aux)];
+  end
+end
+
---
+++ lib/ConfidenceIntervals/run_CI.m
@@ -0,0 +1,79 @@
+%% <../run_CI.m *run_CI*>
+% created by Dina Lika 2018/09/17
+%
+% 1. Calibration step for the interval estimation
+%          calls get_lf_distribution
+%          saves output to calibrate.mat
+%
+% 2. Calculates the profile of the loss function for a parameter
+%          calls get_profile
+%          saves output to profile_nameOFparameter
+%
+% 3. Plots the profile of the loss function for a parameter;
+%          plots the distribution function of the loss function of Monte Carlo simulations
+%          gives the confidence interval of the parameter
+%
+% The working folder must contain the 3 user-defined files of your species
+% mydata_my_pet.m, predict_my_pet.m, pars_init_my_pet.m (with the best estimates)
+%
+% The theory is discussed in Marques et al. 2018. "Fitting Multiple
+% Models to Multiple Data Sets". J Sea Research, doi.org/10.1016/j.seares.2018.07.004
+
+close all
+global pet
+%% The user must specify lines 22-42
+pet = 'my_pet';  % replace my_pet, with your species name
+
+% input for the calibration step
+nbTrials = 500;   % number of data sets
+nCont  = 4;     % increase number of continuations, if necessary
+
+% parameter for the profile step
+pProfile ='v';    % name of the parameter for which the profile will be calculated
+lowVal = 0.005;   % lower value of the profile interval, depends on the parameter
+upperVal = 0.04;  % upper value of the profile interval, depends on the parameter
+dim = 10; % increase for a smooth profile
+
+% Specify the confidence level for the confidence interval
+clevel =0.9; % e.g. 0.9 for a 90% confidence interval
+
+% stepCI - Choose the step(s) you want to simulate
+%          0: runs the whole procedure; 
+%          1: runs only the calibration step
+%          2: runs only the profile for a parameter step
+%          3: computes the CI and graphs the profile
+stepCI = 0; 
+
+%%
+if stepCI == 0 || stepCI == 1
+    fprintf('\n Step 1. Calibration\n\n');
+    [lf_calibrate, pars_calibrate, name_par] = get_lf_distribution(nbTrials, nCont);
+    % lf_calibrate: vector with the values of the loss function
+    % pars_calibrate: matrix with the parameter values 
+    %                 rows: free parameters; columns: number of datasets
+    % name_par: names of free parameters
+    save('calibrate', 'lf_calibrate', 'pars_calibrate', 'name_par')
+end
+%
+if stepCI == 0 || stepCI == 2
+    fprintf('\n Step 2. Profile for parameter %s \n\n', pProfile);
+    [lf_profile, pars_profile] = get_profile(pProfile,lowVal,upperVal,dim,nCont);
+    % lf_profile: vector with the values of the loss function
+    % pars_profile: vector with the parameter values 
+
+    save(['profile_',pProfile], 'lf_profile', 'pars_profile')
+end
+%
+if  stepCI == 0 || stepCI == 3
+    fprintf('\n Step 3. Show plots and CI\n\n');
+    if exist(['profile_',pProfile], 'file') == 0 && exist('calibrate.mat', 'file')== 0
+        disp('Execute steps 1 and 2 first')
+    else
+        load(['profile_',pProfile])
+        load('calibrate.mat')
+    	[lf_thres, ci_low, ci_upper] = plot_profile_CI(pars_profile, lf_profile, lf_calibrate, clevel);
+        % confidence interval
+        fprintf('The CI for %s is from %1.4f to %1.4f \n', pProfile, ci_low, ci_upper)
+        fprintf('The threshold value for the loss function is %1.3f \n', lf_thres)      
+    end
+end
