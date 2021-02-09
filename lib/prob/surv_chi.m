810cab6e5e6e9cd9f6e9615e0f50a31850a0e72d
Dina Lika <DinaLika@users.noreply.github.com>
2021-02-10 00:15:13 +0200

    error correction in surv_ch
    error in the call of gammainc

--- lib/prob/surv_chi.m
+++ lib/prob/surv_chi.m
@@ -3,7 +3,7 @@
 
 %%
 function p = surv_chi (nu, x)
-  % created 2002/04/09 by Bas Kooijman
+  % created 2002/04/09 by Bas Kooijman, modified 2021/02/10 by Dina Lika
   
   %% Syntax
   % p = <../surv_chi.m *surv_chi*> (nu, x)
@@ -26,7 +26,6 @@ function p = surv_chi (nu, x)
   %% Example of use
   % surv_chi(2,[3 4 6.5])
 
-  
   x = reshape(x, length(x), 1);
-  p = 1 - gammainc(nu/2,x/2);
+  p = 1 - gammainc(x/2,nu/2);
 
