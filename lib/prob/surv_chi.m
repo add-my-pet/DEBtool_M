bbcb43484b0c47a5951e7c6f73887a48cb8e9d92
baskooijman <bas.kooijman@vu.nl>
2021-02-10 08:26:28 +0100

    Update surv_chi.m

--- lib/prob/surv_chi.m
+++ lib/prob/surv_chi.m
@@ -3,11 +3,7 @@
 
 %%
 function p = surv_chi (nu, x)
-<<<<<<< HEAD
-  % created 2002/04/09 by Bas Kooijman; modified 2021/02/10
-=======
   % created 2002/04/09 by Bas Kooijman, modified 2021/02/10 by Dina Lika
->>>>>>> 810cab6e5e6e9cd9f6e9615e0f50a31850a0e72d
   
   %% Syntax
   % p = <../surv_chi.m *surv_chi*> (nu, x)
@@ -30,10 +26,5 @@ function p = surv_chi (nu, x)
   %% Example of use
   % surv_chi(2,[3 4 6.5])
 
-<<<<<<< HEAD
   p = 1 - gammainc(x(:)/2,nu/2);
-=======
-  x = reshape(x, length(x), 1);
-  p = 1 - gammainc(x/2,nu/2);
->>>>>>> 810cab6e5e6e9cd9f6e9615e0f50a31850a0e72d
 
