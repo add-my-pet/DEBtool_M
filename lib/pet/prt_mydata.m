22d471a18e93f0c540f9249c5040986137dca097
baskooijman <bas.kooijman@vu.nl>
2021-05-21 07:25:29 +0200

    prt_mydata

--- lib/pet/prt_mydata.m
+++ lib/pet/prt_mydata.m
@@ -327,23 +327,23 @@ fclose(fid);
 
 end
 
-% function str = cell2str(cell)
-%   if isempty(cell)
-%     str = '{}'; return
-%   elseif ischar(cell)
-%     str = ['{''', cell, '''}']; return
-%   end
-%   n = length(cell);
-%   if n == 1
-%     str = ['{''', cell{1}, '''}']; return
-%   else
-%     str = '{';
-%     for i=1:n
-%       str = [str,'''',cell{i}, ''','];
-%     end
-%     str(end) = '}';
-%   end
-% end
+function str = cell2str(cell)
+  if isempty(cell)
+    str = '{}'; return
+  elseif ischar(cell)
+    str = ['{''', cell, '''}']; return
+  end
+  n = length(cell);
+  if n == 1
+    str = ['{''', cell{1}, '''}']; return
+  else
+    str = '{';
+    for i=1:n
+      str = [str,'''',cell{i}, ''','];
+    end
+    str(end) = '}';
+  end
+end
 
 function str = vec2str(vec)
 n = length(vec); str = '[';
