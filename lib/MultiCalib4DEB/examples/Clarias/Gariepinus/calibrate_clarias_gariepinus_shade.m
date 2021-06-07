5a5173ffcd7fff68f6e96ede207c475ce2074b34
baskooijman <bas.kooijman@vu.nl>
2021-06-07 12:58:00 +0200

    Update calibrate_clarias_gariepinus_shade.m

--- lib/MultiCalib4DEB/examples/Clarias/Gariepinus/calibrate_clarias_gariepinus_shade.m
+++ lib/MultiCalib4DEB/examples/Clarias/Gariepinus/calibrate_clarias_gariepinus_shade.m
@@ -9,7 +9,7 @@ check_my_pet(pets); % Check pet consistence
 
 % Setting estimation options such as: 
 estim_options('default');
-estim_options('method','ea');
+estim_options('method','mmea');
 estim_options('results_output',5);
 
 estim_options('num_results', 200);
@@ -17,8 +17,8 @@ estim_options('gen_factor', 0.3);
 estim_options('refine_best', 1);
 estim_options('bounds_from_ind', 1); 
 %estim_options('max_calibration_time', 2);
-estim_options('verbose', 0); 
-estim_options('verbose_options', 8); 
+estim_options('verbose', 1); 
+estim_options('refine_running', 1); 
 estim_options('results_display', 'Complete');
 estim_options('ranges', {'z', 0.25}); % For a factor to the original parameter value. 
 estim_options('ranges', {'f_tW', [0.1, 0.4]}); % For a desired range values. 
