63640e78fe5d62cfeaf56c2601710682f0940fb2
baskooijman <bas.kooijman@vu.nl>
2021-07-11 08:36:15 +0200

    plot3i

---
+++ lib/misc/plot3i.m
@@ -0,0 +1,42 @@
+%% plot3i
+% plot points in 3D where markers are specified of each point
+
+%%
+function [Hfig, Hleg] = plot3i(data, legend, txt)
+% created 2021/06/10 by Bas Kooijman
+
+%% Syntax
+% [Hfig, Hleg] = <../plot3i.m *plot3i*> (data, legend, txt) 
+
+%% Description
+% plot data in 3D, using data-point-specific markers
+%
+% Input:
+%
+% * data: (n,3)-array with data points
+% * legend: (n,2)-array with legend
+% * txt: optional string with title
+%
+% Output:
+%
+% * no output
+
+if ~exist('txt','var')
+  txt = [];
+end
+
+Hfig = figure;
+hold on
+n = size(data,1);
+for i=1:n
+    marker = legend{i,1}; T = marker{1}; MS = marker{2}; LW = marker{3}; MEC = marker{4}; MFC = marker{5};  
+    plot3(data(i,1), data(i,2), data(i,3), T, 'MarkerSize', MS, 'LineWidth', LW, 'MarkerFaceColor', MFC, 'MarkerEdgeColor', MEC);
+end
+title(txt);
+
+h = datacursormode(Hfig);
+h.UpdateFcn = @(obj, event_obj)xylabels(obj, event_obj, legend(:,2), data);
+datacursormode on % mouse click on plot
+
+Hleg = shlegend(legend);
+
