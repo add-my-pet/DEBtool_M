86d4a0c8ef358e8eea8742389d694929b626adea
Bas Kooijman <bas.kooijman@vu.nl>
2022-01-07 10:21:18 +0100

    Update mydata_sod.m

--- lib/misc/mydata_sod.m
+++ lib/misc/mydata_sod.m
@@ -4,12 +4,15 @@
 
 n = 10; % number of species
 traits = rand(n,2); % trait values
-d = sod(traits,[],1); % scaled outlier distances
+d = sod(traits); % scaled outlier distances
 
 
 color = color_lava(d/(1.1*max(d))); % colors for species
-marker = cell(n,1); 
+marker = cell(n,1); txt = cell(n,1);
  for i=1:n
    marker{i} = {'o', 8, 5, color(i,:),  color(i,:)};
+   txt{i} = num2str(i);
  end
-plot2i(traits, marker, 'scaled outlier distance');
+plot2i(traits, [marker, txt], 'scaled outlier distance');
+
+
