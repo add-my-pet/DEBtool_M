b6519050b208fcc10e1e309b5222e741d1d06584
Bas Kooijman <bas.kooijman@vu.nl>
2022-02-04 17:33:11 +0100

    Update prt_report_AmPtox.m

--- lib/pet/prt_report_AmPtox.m
+++ lib/pet/prt_report_AmPtox.m
@@ -2,18 +2,18 @@
 % writes report of local results file
 
 %%
-function prt_report_AmPtox(fileNm)
+function prt_report_AmPtox(title)
 % created 2022/02/01 by Bas Kooijman
 
 %% Syntax
-% <../prt_report_AmPtox.m *prt_report_AmPtox*> (varargin) 
+% <../prt_report_AmPtox.m *prt_report_AmPtox*> (title) 
 
 %% Description
 % Writes report in html from the local results file
 %
 % Input:
 %
-% * varargin: optional name of the results_*.mat file
+% * title: optional name of the results_title.mat file
 %
 % Output:
 %
@@ -24,12 +24,10 @@ function prt_report_AmPtox(fileNm)
   global pets
   
   path2sys = [set_path2server, 'add_my_pet/sys/'];
-  if ~exist('fileNm', 'var')
-    %list = cellstr(ls); title = list(contains(list,'.mat')); title = title{1}; load(title) % load the results_*.mat file
-    %title(1:8) = []; title(end-3:end) = []; % select identifying part of the name 
+  if ~exist('title', 'var')
     title = pets{1}; load(['results_',title]);
   else
-    title = fileNm; load(['results_',fileNm]);   
+    load(['results_',title]);   
   end
   
   [~, ~, ~, txtData] = feval(['mydata_', title]); % get txtData
@@ -65,6 +63,7 @@ function prt_report_AmPtox(fileNm)
     fprintf(oid, '    div.right {\n');
     fprintf(oid, '      width: 55%%;\n');
     fprintf(oid, '      float: right;\n');
+    fprintf(oid, '      padding-top: 60px;\n');
     fprintf(oid, '    }\n\n');
 
     fprintf(oid, '    p.clear {\n');
@@ -78,6 +77,83 @@ function prt_report_AmPtox(fileNm)
     fprintf(oid, '      font-size: 0.8em;\n');
     fprintf(oid, '    }\n\n');
 
+    fprintf(oid, '    /*-------- Figs -----------*/\n\n');
+
+    fprintf(oid, '    /* not-enlarged on page */\n');
+    fprintf(oid, '    .myImg {\n');
+    fprintf(oid, '      max-width:300px;\n');
+    fprintf(oid, '      max-height:200px;\n');
+    fprintf(oid, '    }\n\n');
+
+    fprintf(oid, '    /* enlarge figs: to Trigger the Modal */\n');
+    fprintf(oid, '    .myImg {\n');
+    fprintf(oid, '      border-radius: 5px;\n');
+    fprintf(oid, '      cursor: pointer;\n');
+    fprintf(oid, '      transition: 0.3s;\n');
+    fprintf(oid, '    }\n\n');
+
+    fprintf(oid, '    .myImg:hover {opacity: 0.7;}\n\n');
+
+    fprintf(oid, '    /* The Modal (background) */\n');
+    fprintf(oid, '    .modal {\n');
+    fprintf(oid, '      display: none; /* Hidden by default */\n');
+    fprintf(oid, '      position: fixed; /* Stay in place */\n');
+    fprintf(oid, '      z-index: 1; /* Sit on top */\n');
+    fprintf(oid, '      padding-top: 100px; /* Location of the box */\n');
+    fprintf(oid, '      left: 0;\n');
+    fprintf(oid, '      top: 0;\n');
+    fprintf(oid, '      width: 100%%; /* Full width */\n');
+    fprintf(oid, '      height: 100%%; /* Full height */\n');
+    fprintf(oid, '      overflow: auto; /* Enable scroll if needed */\n');
+    fprintf(oid, '      background-color: rgb(0,0,0); /* Fallback color */\n');
+    fprintf(oid, '      background-color: rgba(0,0,0,0.9); /* Black w/ opacity */\n');
+    fprintf(oid, '    }\n\n');
+
+    fprintf(oid, '    /* Modal Content (Image) */\n');
+    fprintf(oid, '    .modal-content {\n');
+    fprintf(oid, '      margin: auto;\n');
+    fprintf(oid, '      display: block;\n');
+    fprintf(oid, '      height: 80%%;\n');
+    fprintf(oid, '      max-height: 700px;\n');
+    fprintf(oid, '    }\n\n');
+
+    fprintf(oid, '    /* Caption of Modal Image (Image Text) - Same Width as the Image */\n');
+    fprintf(oid, '    #caption {\n');
+    fprintf(oid, '      margin: auto;\n');
+    fprintf(oid, '      display: block;\n');
+    fprintf(oid, '      width: 80%%;\n');
+    fprintf(oid, '      max-width: 700px;\n');
+    fprintf(oid, '      text-align: center;\n');
+    fprintf(oid, '      color: #ccc;\n');
+    fprintf(oid, '      padding: 10px 0;\n');
+    fprintf(oid, '      height: 150px;\n');
+    fprintf(oid, '    }\n\n');
+
+    fprintf(oid, '    /* Add Animation - Zoom in the Modal */\n');
+    fprintf(oid, '    .modal-content, #caption {\n');
+    fprintf(oid, '      -webkit-animation-name: zoom;\n');
+    fprintf(oid, '      -webkit-animation-duration: 0.6s;\n');
+    fprintf(oid, '      animation-name: zoom;\n');
+    fprintf(oid, '      animation-duration: 0.6s;\n');
+    fprintf(oid, '    }\n\n');
+   
+    fprintf(oid, '    @-webkit-keyframes zoom {\n');
+    fprintf(oid, '      from {-webkit-transform:scale(0)}\n');
+    fprintf(oid, '      to {-webkit-transform:scale(1)}\n');
+    fprintf(oid, '    }\n\n');
+
+    fprintf(oid, '    @keyframes zoom {\n');
+    fprintf(oid, '      from {transform:scale(0)}\n');
+    fprintf(oid, '      to {transform:scale(1)}\n');
+    fprintf(oid, '    }\n\n');
+
+    fprintf(oid, '    .close:hover,\n');
+    fprintf(oid, '    .close:focus {\n');
+    fprintf(oid, '      color: #bbb;\n');
+    fprintf(oid, '      text-decoration: none;\n');
+    fprintf(oid, '      cursor: pointer;\n');
+    fprintf(oid, '    }\n\n');
+
     fprintf(oid, '  </style>\n');
     fprintf(oid, '</head>\n\n');
     
@@ -87,7 +163,7 @@ function prt_report_AmPtox(fileNm)
     fprintf(oid, '  <div class="left">\n');
     fprintf(oid, '  <h1>%s</h1>\n', title);
     flds = fields(metaData); 
-    flds = flds(~ismember(flds,{'biblist','discussion','facts','acknowledgment','data_0','data_1','data_2','bibkey'}));
+    flds = flds(~ismember(flds,{'biblist','discussion','facts','acknowledgment','data_0','data_1','data_2','bibkey','COMPLETE'}));
     n_flds = length(flds);
     for i=1:n_flds
        txt = metaData.(flds{i}); if isnumeric(txt); txt = num2str(txt); end
@@ -121,13 +197,37 @@ function prt_report_AmPtox(fileNm)
     fprintf(oid, '  <div class="right">\n');
     png = list(contains(list,'.png')); png = png(contains(png,title)); n_png = length(png);
     for i = 1:n_png
-      if ~contains(png{i},'legend')
-        fprintf(oid, '    <img src=%s width="500px">\n',png{i});
-      else
-        fprintf(oid, '    <img src=%s width="150px">\n',png{i});
-     end
+      fprintf(oid, '    <img class="myImg" src=%s alt="">\n',png{i});
     end
+    fprintf(oid, '    \n');
+   
+    fprintf(oid, '    <div id="myModal" class="modal">\n');        
+    fprintf(oid, '      <img class="modal-content" id="img01">\n');
+    fprintf(oid, '      <div id="caption"></div>\n');
+    fprintf(oid, '    </div>\n');
     fprintf(oid, '  </div>\n\n');
+
+    fprintf(oid, '  <script>\n');
+    fprintf(oid, '    var modal = document.getElementById("myModal");\n');
+    fprintf(oid, '    var i;\n\n');
+
+    fprintf(oid, '    var img = document.getElementsByClassName("myImg");\n');
+    fprintf(oid, '    var modalImg = document.getElementById("img01");\n');
+    fprintf(oid, '    var captionText = document.getElementById("caption");\n\n');
+
+    fprintf(oid, '    for(i=0;i< img.length;i++) {\n');    
+    fprintf(oid, '      img[i].onclick = function(){\n');
+    fprintf(oid, '        modal.style.display = "block";\n');
+    fprintf(oid, '        modalImg.src = this.src;\n');
+    fprintf(oid, '        captionText.innerHTML = this.alt;\n');  
+    fprintf(oid, '      }\n');
+    fprintf(oid, '    }\n\n');
+
+    fprintf(oid, '    // When the user clicks on modelImg, close the modal\n');
+    fprintf(oid, '    modalImg.onclick = function() {\n'); 
+    fprintf(oid, '      modal.style.display = "none";\n');
+    fprintf(oid, '    }\n');
+    fprintf(oid, '  </script>\n\n');
     
     fprintf(oid, '  <p class="clear"></p>\n\n');
 
@@ -191,6 +291,8 @@ function prt_report_AmPtox(fileNm)
       fprintf(oid,'     </ul>\n\n'); % close the unordered list      
     end
   
+    fprintf(oid, '  <hr>\n\n');
+    
     % footer
     fprintf(oid, '  <div class="footer">\n');
     fprintf(oid, '    Report generated by <a href="https://add-my-pet.github.io/AmPtox/docs/">AmPtox</a>\n');
