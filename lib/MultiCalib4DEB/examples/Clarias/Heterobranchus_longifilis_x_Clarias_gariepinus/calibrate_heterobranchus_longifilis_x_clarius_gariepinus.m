a969e6f1adfecb98ff6136c1169f56bfd92e3ef7
JuanfranRobles <juanfrarobles@gmail.com>
2022-03-28 00:05:50 +0200

    Update calibration species examples

--- lib/MultiCalib4DEB/examples/Clarias/Heterobranchus_longifilis_x_Clarias_gariepinus/calibrate_heterobranchus_longifilis_x_clarius_gariepinus.m
+++ lib/MultiCalib4DEB/examples/Clarias/Heterobranchus_longifilis_x_Clarias_gariepinus/calibrate_heterobranchus_longifilis_x_clarius_gariepinus.m
@@ -10,29 +10,19 @@ pets = {'Heterobranchus_longifilis_x_Clarius_gariepinus'};
 check_my_pet(pets); 
 
 % Setting estimation options such as: 
-% Loss function, method to use, filter, etc
 estim_options('default');
-% Setting 'mm' (mmultimodal) for calibration 
-estim_options('method', 'mm1');
-% Setting calibration options (number of runs, maximum function
-% evaluations, ...) 
-calibration_options('default'); 
-% Set number of evaluations
-calibration_options('max_fun_evals', 40000);
-% Set value for individual generation ranges
-calibration_options('gen_factor', 0.3); 
-% Set if the initial guest from data is introduced into initial population
-calibration_options('add_initial', 1); 
-% Set if the best individual found will be refined with a local search
-% method
-calibration_options('refine_best', 1);
-% Set if the bounds for the algorithm's first individuals population is
-% taken from the data values or from pseudodata (1 -> data | 0 ->
-% pseudodata average values)
-calibration_options('bounds_from_ind', 1); 
-% Verbose options
-% Activate verbose
-calibration_options('verbose', 1); 
-calibration_options('verbose_options', 8); 
-% Calibrate
-[best, out, best_favl] = calibrate;
+estim_options('method','mmea');
+estim_options('results_output',5);
+
+estim_options('num_results', 50);
+estim_options('gen_factor', 2); 
+estim_options('activate_niching', 1); 
+estim_options('sigma_share', 0.15);
+estim_options('bounds_from_ind', 1); 
+estim_options('min_convergence_threshold', 1e-4);
+estim_options('verbose', 1); 
+estim_options('verbose_options', 5);
+estim_options('results_display', 'Complete');
+estim_options('save_results',1)
+
+[nsteps, info, fval] = estim_pars;
