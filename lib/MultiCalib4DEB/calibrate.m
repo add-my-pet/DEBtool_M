68b134a2d3a66b15db8d3197458c2a8cae99aae3
JuanfranRobles <juanfrarobles@gmail.com>
2022-04-02 21:04:24 +0200

    Update calibrate and MMEA algorithms to directly discard the solutions that do not pass the filters instead of trying to fix them

--- lib/MultiCalib4DEB/calibrate.m
+++ lib/MultiCalib4DEB/calibrate.m
@@ -141,39 +141,4 @@ function [best_sol, result, best_fval] = calibrate
       else
       end
    end
-
-   % Save the best results data into solution set for printing or working
-   % with them by using DEB modules.
-   % results_pets(best_sol, metaPar, txtPar, data, auxData, metaData, txtData, weights);
-   result = save_results(result, par, metaPar, txtPar, data, auxData, metaData, txtData, weights);
-    %% Save solutions found for a latter analysis
-   if exist('result', 'var') && ~isempty(result)
-      if strcmp(results_filename, 'Default') ~= 0
-         dtime = strsplit(datestr(datetime), ' ');
-         auxtime = dtime{2}; auxtime([3 6 9]) = 'hms'; % Changing ':' time separators to h/m/s
-         filename = ['solutionSet_', pets{1}, '_', dtime{1}, '_', auxtime]; % Filename with calibrated pet name and date; removed: strcat
-      else
-         filename = results_filename;
-      end
-      save(filename, 'result');
-      disp('CALIBRATION PROCESS FINISHED CORRECTLY');
-   end
-   
-   %% Print results depeding on results output parameter
-   plot_results(result, ..., 
-            result.solutionSet.txtPar, result.solutionSet.data, ...,
-            result.solutionSet.auxData, metaData, ..., 
-            result.solutionSet.txtData, weights, results_display);
-   
-   %% check filter
-   parPets = parGrp2Pets(best_sol); % convert parameter structure of group of pets to cell string for each pet
-   if filter
-      for i = 1:n_pets
-         if iscell(metaPar.model)
-            feval(['warning_', metaPar.model{i}], parPets.(pets{i}));
-         else
-            feval(['warning_', metaPar.model], parPets.(pets{i}));
-         end
-      end
-   end
 end
