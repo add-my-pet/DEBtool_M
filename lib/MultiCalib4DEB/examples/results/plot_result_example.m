3c2e6214bd64a8601c0c7aadd51679c3d9b99a4d
JuanfranRobles <juanfrarobles@gmail.com>
2022-04-02 23:54:35 +0200

    Update plot_chart and plot_result functions

--- lib/MultiCalib4DEB/examples/results/plot_result_example.m
+++ lib/MultiCalib4DEB/examples/results/plot_result_example.m
@@ -1,18 +1,2 @@
-global pets 
-
-% The pet to calibrate
-pets = {'Dipodomys_deserti'};
-% Check pet consistence
-check_my_pet(pets);
-
-% Get pet data
-[data, auxData, metaData, txtData, weights] = mydata_pets;
-
-% Load the solution set (example for Clarias Gariepinus). 
-%load('solutionSet_Clarias_gariepinus_20-Apr-2021_20:42:00.mat')
-
 % Plot the solutions!
-plot_results(result, ..., 
-            result.solutionSet.txtPar, result.solutionSet.data, ...,
-            result.solutionSet.auxData, metaData, ..., 
-            result.solutionSet.txtData, weights, 'Set');
+plot_results('results_Lobatus_gigas_mmea', 'Set');
