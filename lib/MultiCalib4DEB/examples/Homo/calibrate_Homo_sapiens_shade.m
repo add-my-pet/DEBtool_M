e7222773e36c34953a61d2b67cdbbce28c03e969
JuanfranRobles <juanfrarobles@gmail.com>
2022-06-22 11:00:52 +0200

    Update the species calibrated in the MultiCalib4DEB examples

--- lib/MultiCalib4DEB/examples/Homo/calibrate_Homo_sapiens_shade.m
+++ lib/MultiCalib4DEB/examples/Homo/calibrate_Homo_sapiens_shade.m
@@ -5,7 +5,7 @@ close all;
 global pets
 
 % The pet to calibrate
-pets = {'Porcellio_scaber'};
+pets = {'Homo_sapiens'};
 % Check pet consistence
 check_my_pet(pets); 
 
@@ -14,12 +14,14 @@ estim_options('default');
 estim_options('method','mmea');
 estim_options('results_output',5);
 
-estim_options('num_results', 50);
-estim_options('gen_factor', 2); 
+estim_options('num_runs', 20);
+estim_options('num_results', 100);
+estim_options('gen_factor', 0.8);
+estim_options('factor_type', 'mult');
 estim_options('activate_niching', 1); 
-estim_options('sigma_share', 0.15);
+estim_options('sigma_share', 0.05);
 estim_options('bounds_from_ind', 1); 
-estim_options('min_convergence_threshold', 1e-4);
+estim_options('max_calibration_time', 10);
 estim_options('verbose', 1); 
 estim_options('verbose_options', 5);
 estim_options('results_display', 'Complete');
--- lib/MultiCalib4DEB/examples/Lobatus/calibrate_Lobatus_gigas_shade.m
+++ lib/MultiCalib4DEB/examples/Lobatus/calibrate_Lobatus_gigas_shade.m
@@ -1,11 +1,11 @@
-%% Calibrates the Clarias Gariepinus pet using SHADE multimodal algorithm
+%% Calibrates the Lobatus gigas pet using SHADE multimodal algorithm
 %% 
 
 close all; 
 global pets
 
 % The pet to calibrate
-pets = {'Lepus_timidus'};
+pets = {'Lobatus_gigas'};
 % Check pet consistence
 check_my_pet(pets); 
 
@@ -14,12 +14,14 @@ estim_options('default');
 estim_options('method','mmea');
 estim_options('results_output',5);
 
-estim_options('num_results', 50);
-estim_options('gen_factor', 2); 
+estim_options('num_runs', 20);
+estim_options('num_results', 100);
+estim_options('gen_factor', 0.8);
+estim_options('factor_type', 'mult');
 estim_options('activate_niching', 1); 
-estim_options('sigma_share', 0.15);
+estim_options('sigma_share', 0.05);
 estim_options('bounds_from_ind', 1); 
-estim_options('min_convergence_threshold', 1e-4);
+estim_options('max_calibration_time', 10);
 estim_options('verbose', 1); 
 estim_options('verbose_options', 5);
 estim_options('results_display', 'Complete');
