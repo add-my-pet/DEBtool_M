e2ad81382dbbe9af7518dca4d69eccc55d325367
Bas Kooijman <bas.kooijman@vu.nl>
2022-07-31 07:56:37 +0200

    Update add_noise.m

--- lib/misc/add_noise.m
+++ lib/misc/add_noise.m
@@ -3,7 +3,7 @@
 
 %%
 function xNoise = add_noise (x, cv, nr)
-  % created by Dina Lika 2016/06/29; modified by Goncalo Marques 2016/07/16
+  % created by Dina Lika 2016/06/29; modified by Goncalo Marques 2016/07/16, Bas Kooijman 2022/07/30
    
   %% Syntax 
   % <../add_noise.m *add_noise*>
@@ -13,19 +13,26 @@ function xNoise = add_noise (x, cv, nr)
   %
   % Input
   %
-  % * x: scalar with data
+  % * x: scalar or vector with data
   % * cv: scalar with coefficient of variation
-  % * nr: scalar with number of samples to average over
+  % * nr: optional scalar with number of samples to average over (default 1)
   %  
   % Output
   %
-  % * x_noise: vector with data + noise
+  % * x_noise: vector or matrix with data + noise
   
   %% Remarks
   % nr serves to mimick an average over several samples
   % Be aware that by increasing nr the real cv of the result decreases
 
-  xNoise = mean(gen_lognormal(x, (cv * x)^2, nr));
+  if ~exist('nr','var')
+    nr = 1;
+  end
+  
+  n = length(x); xNoise = zeros(n,1);
+  for i=1:n
+   xNoise(i) = mean(gen_lognormal(x(i), (cv * x(i))^2, nr));
+  end
 end
 
 function val = gen_lognormal(m, v, N)
