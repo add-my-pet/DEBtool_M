3f2986858a39cd856e870638a0cfab154b3d64d1
Bas Kooijman <bas.kooijman@vu.nl>
2023-07-23 09:23:02 +0200

    addpseudodata

--- lib/pet/addpseudodata.m
+++ lib/pet/addpseudodata.m
@@ -2,8 +2,8 @@
 % Adds pseudodata information into inputed data structures 
 
 %%
-function [data, units, label, weight] = addpseudodata(data, units, label, weight)
-% created 2015/01/16 by Goncalo Marques and Bas Kooijman, 2018/08/26 Bas Kooijman
+function [data, units, label, weights] = addpseudodata(data, units, label, weights)
+% created 2015/01/16 by Goncalo Marques and Bas Kooijman, 2018/08/26, 2023/07/23 Bas Kooijman
 
 %% Syntax
 % [data, units, label, weight] = <../addpseudodata.m *addpseudodata*> (data, units, label, weight)
@@ -16,14 +16,14 @@ function [data, units, label, weight] = addpseudodata(data, units, label, weight
 % * data : structure with data values 
 % * units : structure with data units 
 % * label : structure with data labels 
-% * weight : structure with data weights for a regression 
+% * weights : structure with data weights for a regression 
 %
 % Output: 
 %
 % * data : structure with data and pseudodata values 
 % * units : structure with data and pseudodata units 
 % * label : structure with data and pseudodata labels 
-% * weight : structure with data and pseudodata weights for a regression 
+% * weights : structure with data and pseudodata weights for a regression 
 
 %% Example of use
 % [data, units, label, weight] = addpseudodata([], [], [], []);
@@ -36,25 +36,23 @@ data.psd.v = 0.02;     units.psd.v = 'cm/d';       label.psd.v = 'energy conduct
 data.psd.kap = 0.8;    units.psd.kap = '-';        label.psd.kap = 'allocation fraction to soma';
 data.psd.kap_R = 0.95; units.psd.kap_R = '-';      label.psd.kap_R = 'reproduction efficiency';
 data.psd.p_M = 18;     units.psd.p_M = 'J/d.cm^3'; label.psd.p_M = 'vol-spec som maint';   
-data.psd.k_J = 0.002;  units.psd.k_J = '1/d';      label.psd.k_J = 'maturity maint rate coefficient';
+data.psd.k = 0.3;      units.psd.k = '-';          label.psd.k = 'maintenance ratio';
 data.psd.kap_G = 0.8;  units.psd.kap_G = '-';      label.psd.kap_G = 'growth efficiency'; 
 
 % set weights
-weight.psd = setweights(data.psd, []);
-weight.psd.v     = 0.1 * weight.psd.v;
-weight.psd.kap   = 0.1 * weight.psd.kap;
-weight.psd.kap_R = 0.1 * weight.psd.kap_R;
-weight.psd.kap_G = 0.1 * weight.psd.kap_G;
-weight.psd.p_M   = 0.1 * weight.psd.p_M;
-weight.psd.k_J   = 0.1 * weight.psd.k_J;
-
-weight.psd.kap_G = 200 * weight.psd.kap_G;   % more weight to kap_G
+weights.psd = setweights(data.psd, []);
+weights.psd.v     = 0.1 * weights.psd.v;
+weights.psd.kap   = 0.1 * weights.psd.kap;
+weights.psd.kap_R = 0.1 * weights.psd.kap_R;
+%weights.psd.kap_G = 0.1 * weights.psd.kap_G;
+weights.psd.p_M   = 0.1 * weights.psd.p_M;
+weights.psd.k     = 0.1 * weights.psd.k;
 
 if strcmp(loss_function, 'su')
-  weight.psd.v     = 10^(-4) * weight.psd.v;
-  weight.psd.p_M   = 10^(-4) * weight.psd.p_M;
-  weight.psd.k_J   = 10^(-4) * weight.psd.k_J;
-  weight.psd.kap_R = 10^(-4) * weight.psd.kap_R;
-  weight.psd.kap   = 10^(-4) * weight.psd.kap;
-  weight.psd.kap_G = 10^(-4) * weight.psd.kap_G;
+  weights.psd.v     = 1e-4 * weights.psd.v;
+  weights.psd.p_M   = 1e-4 * weights.psd.p_M;
+  weights.psd.k     = 1e-4 * weights.psd.k;
+  weights.psd.kap_R = 1e-4 * weights.psd.kap_R;
+  weights.psd.kap   = 1e-4 * weights.psd.kap;
+  weights.psd.kap_G = 1e-4 * weights.psd.kap_G;
 end
