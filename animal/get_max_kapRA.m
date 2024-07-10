d2fef4fdfba442afaf3afad625e4cfb20d3f3c66
Bas Kooijman <bas.kooijman@vu.nl>
2024-07-10 09:42:49 +0200

    get_max_kapRA

--- animal/get_max_kapRA.m
+++ animal/get_max_kapRA.m
@@ -17,7 +17,7 @@ function res = get_max_kapRA(pars,f)
 % 
 % * pars: (n,4)-matrix with parameters in columns:
 % 
-%    - p_Am, J/d.cm^2, spec assim rate
+%    - p_Am, J/d.cm^2, spec assim rate (after accelleration)
 %    - p_M, J/d.cm^3, spec som maint rate
 %    - k_J, 1/d, maturity maint rate coeff
 %    - E_Hp, J, maturity at puberty
@@ -26,11 +26,12 @@ function res = get_max_kapRA(pars,f)
 %
 % Output:
 % 
-% * res: (n,4)-matrix with 
+% * res: (n,5)-matrix with 
 %
-%    - kap_min: minimum value for kap for which kapRA > 0
 %    - kap_opt: value of kap that maximizes kapRA
 %    - kapRA_opt: max value of kapRA
+%    - kap_min: minimum value for kap for which kapRA > 0
+%    - kap_max: maximum value for kap for which kapRA > 0
 %    - info: boolean for numerical success (1) or failure (0)
 
 %% Remarks
@@ -50,23 +51,31 @@ n = size(pars,1); res = NaN(n,4);
 for i = 1:n
   p_Am = pars(i,1); p_M = pars(i,2); k_J = pars(i,3); E_Hp = pars(i,4);
 
+  % prepare for plotting
+  kap = linspace(1e-5,0.99999,500)';
+  L_i = kap * f * p_Am/ p_M; % cm
+  p_Jp = k_J * E_Hp ./ L_i.^3; % J/d.cm^3
+  kapRA = 1 - kap .* (1 + p_Jp/ p_M);
+  kap0 = kap(kapRA>0); kap0 = [kap0(1) kap0(end)];
+
   % min kap
   try
     kapMin = @(kap,k_J,E_Hp,f,p_Am,p_M) 1/kap - 1 - k_J * E_Hp * p_M^2/ (kap*f*p_Am)^3; 
-    [kap_min, ~, infoMin] = fzero(@(kap) kapMin(kap,k_J,E_Hp,f,p_Am,p_M),[1e-10, 0.95]);
-  catch
+    [kap_min, ~, infoMin] = fzero(@(kap) kapMin(kap,k_J,E_Hp,f,p_Am,p_M),kap0(1));
+    [kap_max, ~, infoMax] = fzero(@(kap) kapMin(kap,k_J,E_Hp,f,p_Am,p_M),kap0(2));
+ catch
     infoMin = 1; kap_min = 0;
   end
  
   % max kapRA
   try
     kapOpt = @(kap,k_J,E_Hp,f,p_Am,p_M) 1 + kap - 2 * k_J * E_Hp/ (kap*f*p_Am/p_M)^3/ p_M; 
-    [kap_opt, ~, infoOpt] = fzero(@(kap) kapOpt(kap,k_J,E_Hp,f,p_Am,p_M),[kap_min, 1]);
+    [kap_opt, ~, infoOpt] = fzero(@(kap) kapOpt(kap,k_J,E_Hp,f,p_Am,p_M),[kap_min kap_max]);
     p_Jp_opt = k_J * E_Hp / (kap_opt * f * p_Am/ p_M)^3;
     kapRA_opt = 1 - kap_opt * (1 + p_Jp_opt/ p_M);
   catch
     infoOpt = 0; kap_opt = NaN; kapRA_opt = NaN;
   end
   info = infoMin==1 && infoOpt==1;
-  res(i,1) = kap_min; res(i,2) = kap_opt; res(i,3) = kapRA_opt; res(i,4) = info;
+  res(i,1) = kap_opt; res(i,2) = kapRA_opt; res(i,3) = kap_min; res(i,4) = kap_max; res(i,5) = info;
 end
--- animal/mydata_get_max_kapRA.m
+++ animal/mydata_get_max_kapRA.m
@@ -1,5 +1,10 @@
 %% mydata_get_max_kapRA
 
-nm = select('Rodentia'); res = get_max_kapRA(read_stat(nm,{'p_Am','p_M','k_J','E_Hp'}));
+nm = select('Rodentia'); vars = read_stat(nm,{'p_Am','p_M','k_J','E_Hp','s_M','s_s'}); 
+vars(:,1) = vars(:,1).*vars(:,5); s_s = vars(:,6); res = get_max_kapRA(vars);
 pRA = read_stat(nm,{'p_Ri','p_Ai','kap'}); kapRA = pRA(:,1)./pRA(:,2); kap = pRA(:,3); 
-prt_tab({nm,res,kap,kapRA,kapRA./res(:,3)},{'species','kap_min','kap_opt','kapRA_opt','info','kap', 'kapRA','kapRA/kapRA_opt'})
+prt_tab({nm,res,kap,kapRA,kapRA./res(:,3),s_s},{'species','kap_opt','kapRA_opt','kap_min','kap_max','info','kap','kapRA','kapRA/kapRA_opt','s_s'})
+
+% Pedetes_capensis
+% p_Am = vars(1,1);p_M = vars(1,2);k_J = vars(1,3);E_Hp = vars(1,4); 
+% [kap_opt, kapRA_opt, kap_min, kap_max] = kap_kapRA(p_Am,p_M,k_J,E_Hp)
