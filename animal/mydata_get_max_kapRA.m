9a0a98959e9a94436a4a8bcd89409e5b97096458
Bas Kooijman <bas.kooijman@vu.nl>
2024-07-11 17:33:01 +0200

    Update mydata_get_max_kapRA.m

--- animal/mydata_get_max_kapRA.m
+++ animal/mydata_get_max_kapRA.m
@@ -1,8 +1,9 @@
 %% mydata_get_max_kapRA
 
-nm = 'Glaucomys_volans'; pars = read_stat(nm,{'p_Am','p_M','k_J','E_Hp','s_M','s_s'}); 
-s_s = pars(:,6); res = get_max_kapRA(pars);
+nm = select('Mammalia'); pars = read_stat(nm,{'p_Am','p_M','k_J','E_Hp','s_M','kap'}); 
+res = get_max_kapRA(pars); % res: kap_opt, kapRA_opt, kap_min, kap_max, s_s
 pRA = read_stat(nm,{'p_Ri','p_Ai','kap'}); kapRA = pRA(:,1)./pRA(:,2); kap = pRA(:,3); 
-prt_tab({nm,res,kap,kapRA,kapRA./res(:,2),s_s},{'species','kap_opt','kapRA_opt','kap_min','kap_max','kap','kapRA','kapRA/kapRA_opt','s_s'})
+prt_tab({nm,res(:,1:4),kap,kapRA,kapRA./res(:,2),pRA(:,1),pRA(:,2)},{'species','kap_opt','kapRA_opt','kap_min','kap_max','kap','kapRA','kapRA/kapRA_opt','p_Ri','p_Ai'})
 
-shkapRA(pars(1:4));
+% just for 1 species
+%shkapRA(pars(1:4));
